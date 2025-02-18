54fde6e1c6627a768851773cc78f3bef81029bc2
mARTilda-art <109670365+mARTilda-art@users.noreply.github.com>
2025-02-18 21:36:27 +0100

    added new city

--- javascript/index.js
+++ javascript/index.js
@@ -12,6 +12,19 @@ function updateTime() {
     );
   }
 
+  // Berlin
+  let berlinElement = document.querySelector("#berlin");
+  if (berlinElement) {
+    let berlinDateElement = berlinElement.querySelector(".date");
+    let berlinTimeElement = berlinElement.querySelector(".time");
+    let berlinTime = moment().tz("Europe/Berlin");
+
+    berlinDateElement.innerHTML = berlinTime.format("MMMM Do YYYY");
+    berlinTimeElement.innerHTML = berlinTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
   // Tokyo
   let tokyoElement = document.querySelector("#tokyo");
   if (tokyoElement) {
